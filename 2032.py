3ac6ccec001f55b7d30c37ae1ce978e6b5d6fd27
Andrea Oquendo <oquendo.3455@gmail.com>
2024-04-27 15:01:15 -0300

    2032 started

---
+++ 2032.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoOutOfThree(self, nums1: list[int], nums2: list[int], nums3: list[int]) -> list[int]:
+        pass
+
+s = Solution()
+result = s.twoOutOfThree([], [], [])
+print(result)
+        
