294a4b07ef32977cb769638c329889a80a17424c
Andrea Oquendo <oquendo.3455@gmail.com>
2024-05-04 19:12:49 -0300

    1684 solved

--- 1684.py
+++ 1684.py
@@ -1,3 +1,12 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: list[str]) -> int:
-        pass
+        count = len(words)
+        for word in words:
+            for ch in word:
+                if ch not in allowed:
+                    count-=1
+                    break
+        return count
+
+s = Solution()
+print(s.countConsistentStrings("ab", ["ad","bd","aaab","baa","badab"]))
